22b807224b3a95d7db9805ea12871bc79055e90c
Teodor Hurtigh Isaacs <Teodor.hurtigh@gmail.com>
2017-06-16 13:42:38 +0200

    Ser till att data från teamconfig kommer vidare till nästa view vilket ska vara: TeamGetReadyActivity, men just nu är det Game activity

--- app/src/main/java/com/thi/ppm/medandraord/TeamConfigActivity.java
+++ app/src/main/java/com/thi/ppm/medandraord/TeamConfigActivity.java
@@ -7,9 +7,11 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+
 public class TeamConfigActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.thi.ppm.MESSAGE";
+    public static final String ALL_TEAMS = "com.thi.ppm.allTeams";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,26 +20,55 @@ public class TeamConfigActivity extends AppCompatActivity {
     }
 
     public void teamContinue(View view){
-        /* Alternativ lösning på att skicka typ.
-        //Sending data to another Activity
-                //Starting a new Intent
-                Intent nextScreen = new Intent(getApplicationContext(), SecondScreenActivity.class);
-
-                nextScreen.putExtra("name", inputName.getText().toString());
-                nextScreen.putExtra("email", inputEmail.getText().toString());
-
-                startActivity(nextScreen)
-         */
         Intent intent = new Intent(this, GameActivity.class);
 
-        //TODO get names for the checked boxes
-        /*
         CheckBox teamCheck1 = (CheckBox) findViewById(R.id.teamCheck1);
-        */
-        //TODO Fixa så att team names skickas till nästa screen
-        /*EditText teamName1 = (EditText) findViewById(R.id.teamName1);
-        String message = teamName1.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);*/
+        CheckBox teamCheck2 = (CheckBox) findViewById(R.id.teamCheck2);
+        CheckBox teamCheck3 = (CheckBox) findViewById(R.id.teamCheck3);
+        CheckBox teamCheck4 = (CheckBox) findViewById(R.id.teamCheck4);
+        CheckBox teamCheck5 = (CheckBox) findViewById(R.id.teamCheck5);
+        CheckBox teamCheck6 = (CheckBox) findViewById(R.id.teamCheck6);
+
+        ArrayList<String> teamList = new ArrayList<>();
+
+        if (teamCheck1.isChecked()){
+            EditText teamName1 = (EditText) findViewById(R.id.teamName1);
+            String teamname = teamName1.getText().toString();
+            teamList.add(teamname);
+        }
+        if (teamCheck2.isChecked()){
+            EditText teamName2 = (EditText) findViewById(R.id.teamName2);
+            String teamname = teamName2.getText().toString();
+
+            teamList.add(teamname);
+        }
+        if (teamCheck3.isChecked()){
+            EditText teamName3 = (EditText) findViewById(R.id.teamName3);
+            String teamname = teamName3.getText().toString();
+
+            teamList.add(teamname);
+        }
+        if (teamCheck4.isChecked()){
+            EditText teamName4 = (EditText) findViewById(R.id.teamName4);
+            String teamname = teamName4.getText().toString();
+
+            teamList.add(teamname);
+        }
+        if (teamCheck5.isChecked()){
+            EditText teamName5 = (EditText) findViewById(R.id.teamName5);
+            String teamname = teamName5.getText().toString();
+
+            teamList.add(teamname);
+        }
+        if (teamCheck6.isChecked()){
+            EditText teamName6 = (EditText) findViewById(R.id.teamName6);
+            String teamname = teamName6.getText().toString();
+
+            teamList.add(teamname);
+        }
+
+        intent.putExtra(ALL_TEAMS, teamList);
+
         startActivity(intent);
     }
 }
